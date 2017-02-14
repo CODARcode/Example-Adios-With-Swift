bb73a4d392866af7c2994d2b518e36c1bc17cf9b
Jong Choi <jychoi-hpc@users.noreply.github.com>
2017-02-14 16:33:13 -0500

    Update run-with-dataspaces.sh

--- run-with-dataspaces.sh
+++ run-with-dataspaces.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
-./build.sh
+#./build.sh
 
 rm -f *.bp*
-run-dataspaces.py -s 1 -c 4 : \
+./run-dataspaces.py -s 1 -c 4 --mpirun=aprun : \
     --nompi turbine -n 5 test-adios-staging.tic --wmethod=DATASPACES --rmethod=DATASPACES
